828e81392d388244b275b83a51ccab08b67c8b6c
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-11 12:40:51 +0300

    Define a state slice that contain books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,8 +1,30 @@
+import { v4 as uuidv4 } from 'uuid';
+
 // Actions
 const ADD = 'bookstore/books/ADD_BOOK';
 const REMOVE = 'bookstore/books/REMOVE_BOOK';
 
-const initStateBooks = [];
+// Initial state
+const initStateBooks = [
+  {
+    id: uuidv4(),
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    genere: 'Fiction',
+  },
+  {
+    id: uuidv4(),
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    genere: 'Fiction',
+  },
+  {
+    id: uuidv4(),
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    genere: 'Nonfiction',
+  },
+];
 
 // Reducer
 const ReducerBooks = (state = initStateBooks, action) => {
@@ -11,7 +33,7 @@ const ReducerBooks = (state = initStateBooks, action) => {
       return [...state, action.payload];
 
     case REMOVE:
-      return state.filter((book) => book.id !== action.payload);
+      return state.filter((book) => book.id !== action.payload.id);
 
     default:
       return state;
@@ -19,8 +41,22 @@ const ReducerBooks = (state = initStateBooks, action) => {
 };
 
 // Action creators
-const AddBook = () => ({ type: ADD });
-const RemoveBook = () => ({ type: REMOVE });
+const AddBook = (book) => (
+  {
+    type: ADD,
+    payload: {
+      id: uuidv4(),
+      title: book.title,
+      author: book.author,
+    },
+  });
+
+const RemoveBook = (id) => (
+  {
+    type: REMOVE,
+    payload: { id },
+  }
+);
 
 export { AddBook, RemoveBook };
 export default ReducerBooks;
