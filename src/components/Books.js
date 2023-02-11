b8cd8fcae484e657369784d957e92045ce1bd87f
cherelemma <73167960+cherelemma@users.noreply.github.com>
2023-02-11 18:59:02 +0300

    Fetch books from Bookstore API and display

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,19 +1,31 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useRef } from 'react';
 import Book from './Book';
 import AddBookForm from './NewBook';
+import { FetchBook } from '../redux/books/books';
 
 const Books = () => {
-  const booksList = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const shouldFetchBooks = useRef(true);
+  useEffect(() => {
+    if (shouldFetchBooks.current) {
+      shouldFetchBooks.current = false;
+      dispatch(FetchBook());
+    }
+  }, [dispatch]);
+
+  let bookList = useSelector((state) => state.books).book;
+  if (bookList === null || bookList === undefined) {
+    bookList = [];
+  }
 
   return (
     <div className="books-wrapper">
-      {booksList.map((book) => (
-        <div key={book.id} className="book">
-          {' '}
-          <Book book={book} />
+      {bookList.map((bk) => (
+        <div key={bk.id}>
+          <Book book={bk} />
         </div>
       ))}
-
       <AddBookForm />
     </div>
   );
